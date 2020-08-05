f4b344f829ad2c3f02bae71c102366ffd7fea80d
Lukas Gentele <lg@devspace.cloud>
2020-08-05 06:16:46 -0700

    remove duplicate folder

--- api/pkg/apis/doc.go
+++
@@ -1,6 +0,0 @@
-//go:generate go run ../../vendor/sigs.k8s.io/apiserver-builder-alpha/cmd/apiregister-gen/main.go --input-dirs ./... -h ../../boilerplate.go.txt
-
-//
-// +domain=loft.sh
-
-package apis
